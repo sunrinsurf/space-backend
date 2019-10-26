0cda84f3e2361392f7651b48db3be8f2b2483fd8
Doyeong <jeekc0308@gmail.com>
2019-10-27 04:08:40 +0900

    bug fixed.

--- routes/user/sms/index.js
+++ routes/user/sms/index.js
@@ -23,38 +23,60 @@ function formatPhone(phone) {
   PhoneNumber = "+82" + PhoneNumber.slice(1);
   return PhoneNumber;
 }
-router.post("/", (req, res) => {
-  const { phone } = req.body;
-  if (!phone) {
-    return throwError("휴대폰 번호는 필수입니다.", 400);
+function formatNormalPhone(str) {
+  let data = str.replace(/-/g, "");
+  const [num1, num2, num3] = [
+    data.slice(0, 3),
+    data.slice(3, 7),
+    data.slice(7, 11)
+  ];
+  let n = num1;
+  if (num2) {
+    n += "-" + num2;
   }
+  if (num3) {
+    n += "-" + num3;
+  }
+  return n;
+}
+router.post("/", async (req, res, next) => {
+  try {
+    const { phone } = req.body;
+    if (!phone) {
+      return throwError("휴대폰 번호는 필수입니다.", 400);
+    }
+    const PhoneNumber = formatPhone(phone);
+    console.log(PhoneNumber);
+    const user = await User.findOne({ phone: formatNormalPhone(phone) });
+    if (user) {
+      return throwError("이미 있는 유저입니다.", 422);
+    }
+    const code = process.env.NODE_ENV === 'test' && req.query.code ? req.query.code : getRandomNumber(6);
+    const params = {
+      Message: `Space 인증 번호: [${code}]`,
+      PhoneNumber
+    };
+    const cryptedCode = codeCrypto(code);
 
-  const PhoneNumber = formatPhone(phone);
-  const code = process.env.NODE_ENV === 'test' && req.query.code ? req.query.code : getRandomNumber(6);
-  const params = {
-    Message: `Space 인증 번호: [${code}]`,
-    PhoneNumber
-  };
-  const cryptedCode = codeCrypto(code);
-
-  const chiper = crypto.createCipher("aes-256-gcm", key);
-  const time = process.env.NODE_ENV === 'test' && req.query.time ? parseInt(req.query.time) : new Date().getTime();
-  let token = chiper.update(
-    JSON.stringify({ phone, code: cryptedCode, time }),
-    "utf8",
-    "base64"
-  );
-  token += chiper.final("base64");
-  const handler = process.env.NODE_ENV !== 'test' && sns
-    .publish(params)
-    .promise();
-  Promise.resolve(handler).then(() => {
-    res.json({ success: true, token });
-  })
-    .catch(e => {
+    const chiper = crypto.createCipher("aes-256-gcm", key);
+    const time = process.env.NODE_ENV === 'test' && req.query.time ? parseInt(req.query.time) : new Date().getTime();
+    let token = chiper.update(
+      JSON.stringify({ phone, code: cryptedCode, time }),
+      "utf8",
+      "base64"
+    );
+    token += chiper.final("base64");
+    const handler = process.env.NODE_ENV !== 'test' && sns
+      .publish(params)
+      .promise();
+    try {
+      await Promise.resolve(handler);
+      res.json({ success: true, token });
+    } catch (e) {
       console.error(e);
       throwError("오류가 발생했습니다. 다시 시도해주세요.", 500);
-    });
+    }
+  } catch (e) { next(e); }
 });
 router.put("/:code", (req, res) => {
   const cryptedCode = codeCrypto(req.params.code);
