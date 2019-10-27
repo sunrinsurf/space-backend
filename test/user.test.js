d09b7611850492ae2d78891cec8eaf8274c8230b
root <root@LAPTOP-MNRBSD8U.localdomain>
2019-10-28 08:40:41 +0900

    중복 처리 및 테스트 코드

--- test/user.test.js
+++ test/user.test.js
@@ -2,22 +2,42 @@ const request = require('supertest');
 const app = require('../app');
 const User = require('../models/user');
 
+const userData = {
+    uid: "test01",
+    password: "password",
+    phone: "010-1234-5968",
+    nickname: "테스트",
+    address: "경기도 남양주시",
+    interest: ["기타"]
+};
+function deleteUser(done) {
+    User.deleteOne({ uid: userData.uid })
+        .then(() => {
+            done();
+        })
+        .catch(err => {
+            done(err);
+        });
+}
 describe("/user", function () {
-    after(() => User.deleteMany())
     describe("create", () => {
-        it('should create user', (done) => {
-            request(app)
+        function createUser() {
+            return request(app)
                 .post('/user')
-                .send({
-                    uid: "test01",
-                    password: "password",
-                    phone: "010-1234-5968",
-                    nickname: "테스트",
-                    address: "경기도 남양주시",
-                    interest: ["기타"]
-                })
-                .expect(201)
-                .end(done);
+                .send(userData)
+        }
+        it('should create user', (done) => {
+            createUser().expect(201).end(done);
+        });
+        it("should handle exist user", function (done) {
+            createUser().expect(422).expect({
+                status: 422,
+                message: '이미 존재하는 유저입니다.'
+            }).end(done);
+        });
+        after(function (done) {
+            deleteUser(done);
         });
     });
+
 });
