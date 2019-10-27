9c09101336bf6348d3e823b4cb51324a90f30cb0
Doyeong <jeekc0308@gmail.com>
2019-10-27 13:46:10 +0900

    /auth 완성

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -11,49 +11,42 @@ const User = require('../../models/user');
 
 router.use(bodyParser.json({ extended: true }));
 
-router.post('/', (req, res) => {
-  let tokenExpireTime;
-  if (req.body.remember === true) {
-    // remember == true
-    tokenExpireTime = 604800; // exptime = 7d
-  } else {
-    // remember == false
-    tokenExpireTime = 10800; // exptime = 3h
+router.post('/', async (req, res, next) => {
+  try {
+    const { uid, password } = req.body;
+    const user = await User.findOne({ uid });
+    if (!user) return throwError('아이디가 없거나 비밀번호가 다릅니다.', 403);
+
+    const userSalt = user.enckey;
+    const userPassword = user.password;
+
+    const pbkdf2 = util.promisify(crypto.pbkdf2);
+    const key = await pbkdf2(password, userSalt, 100000, 64, 'sha512');
+
+    const clientPassword = key.toString('base64');
+    if (clientPassword !== userPassword)
+      return throwError('아이디가 없거나 비밀번호가 다릅니다.', 403);
+
+    let tokenExpireTime;
+    if (req.body.remember === true) {
+      // remember == true
+      tokenExpireTime = 604800; // exptime = 7d
+    } else {
+      // remember == false
+      tokenExpireTime = 10800; // exptime = 3h
+    }
+    const payload = {
+      userId: user.uid,
+      username: user.name
+    };
+    const result = jwt.sign(payload, settings.TOKEN_KEY || 'jwt', {
+      expiresIn: tokenExpireTime,
+      issuer: 'surfspace.me'
+    });
+    res.json({ token: result });
+  } catch (e) {
+    next(e);
   }
-  const payload = {
-    userId: req.body.uid,
-    username: req.body.name,
-  };
-  const result = jwt.sign(payload, settings.TOKEN_KEY || 'jwt', {
-    expiresIn: tokenExpireTime,
-    issuer: 'surfspace.me',
-  });
-  res.send(result);
-});
-
-router.post('/login', async (req, res) => {
-  const { uid, password } = req.body;
-  const user = await User.findOne({ uid });
-
-  const userSalt = user.enckey;
-  const userPassword = user.password;
-
-  if (userKey === undefined) {
-    res.send(false);
-  }
-  const pbkdf2 = util.promisify(crypto.pbkdf2);
-
-  const key = await pbkdf2(password, userSalt, 100000, 64, 'sha512');
-
-  const clientPassword = key.toString('base64');
-
-  if (clientPassword !== userPassword) {
-    res.send(false);
-  } else res.send(true);
-});
-
-router.post('/password', (req, res) => {
-  res.send('notUsing');
 });
 
 module.exports = router;
