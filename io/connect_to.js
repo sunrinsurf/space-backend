2424f89b551e2ed5a489596b0c058258710dd7a0
Doyeong <jeekc0308@gmail.com>
2019-11-03 22:15:16 +0900

    chat

--- io/connect_to.js
+++ io/connect_to.js
@@ -1,5 +1,6 @@
 const verifyToken = require('../lib/verifyToken');
 const room = require('./room');
+const User = require('../models/user');
 
 module.exports = (socket, product) => {
   return token => {
@@ -9,10 +10,12 @@ module.exports = (socket, product) => {
     } catch (e) {
       return socket.emit('error', '인증에 실패했습니다.');
     }
-    room.addUser(product, socket.id, user._id, user.nickname);
-    const data = room.getData(product);
+    User.findById(user._id, ['nickname']).then(user => {
+      room.addUser(product, socket.id, user._id, user.nickname);
+      const data = room.getData(product);
 
-    socket.emit('room_data', data);
-    socket.broadcast.emit('room_data', data);
+      socket.emit('room_data', data);
+      socket.broadcast.emit('room_data', data);
+    });
   };
 };
