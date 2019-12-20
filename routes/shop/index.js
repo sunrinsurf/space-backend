7d6ba596058e68f09bf8a1b492fd65a828c81f89
HADMARINE <joonlee031@live.com>
2019-12-20 09:12:30 +0900

    GET /shop/ : modified searching method

--- routes/shop/index.js
+++ routes/shop/index.js
@@ -78,13 +78,13 @@ router.get('/', auth.parseAutorized, async (req, res, next) => {
 
     if (pagination < 1) return throwError('1페이지부터 찾아 주세요.', 400);
     //const userData = req.user && (await User.findById(req.user._id));
-    
-    const search = "(" + req.query.search.replace(" ","|") + ")";
-
-    const query = search
+    const query = req.query.search
       ? {
           title: {
-            $regex: new RegExp(search, 'gi')
+            $regex: new RegExp(
+              '(' + req.query.search.toString().replace(' ', '|') + ')',
+              'i'
+            )
           }
         }
       : {};
